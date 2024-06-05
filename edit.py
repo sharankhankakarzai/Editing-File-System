968634294ad28c5e495370bb980658aaf7628640
sharankhan <sharankhan55555@gmail.com>
2024-06-05 15:09:24 +0000

    edited files 2

---
+++ edit.py
@@ -0,0 +1,15 @@
+ chmod - change mode,to give permission ,chmod -r, chmod -w, chmod -x
+        
+         to change permission or to give permission command is = chmod -x or -w or -r ,file name, enter
+        
+         + = to give all permission
+         
+         permissions = r-read, w-write , x-execute
+
+         user=project manager ,TL=team leader, employee=you
+     
+         touch=to create empty file
+         
+         user     group   other
+         r,w,x    r,w,x   r,w,x      
+   
